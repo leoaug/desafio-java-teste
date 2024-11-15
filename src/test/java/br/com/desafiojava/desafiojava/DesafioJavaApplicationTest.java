625ee9d40e4a2fd1c1a145cc2ce143c8944078dd
Leonardo Silva <leoaug1981@hotmail.com>
2024-11-15 12:38:22 -0300

    implementacao de teste da classe ProjetoService

--- src/test/java/br/com/desafiojava/desafiojava/DesafioJavaApplicationTest.java
+++ src/test/java/br/com/desafiojava/desafiojava/DesafioJavaApplicationTest.java
@@ -11,7 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import br.com.desafiojava.DesafioJavaApplication;
 
 @SpringBootTest
-class DesafioJavaApplicationTests {
+class DesafioJavaApplicationTest {
 
 	private DesafioJavaApplication desafiojavaApplication;
 	
